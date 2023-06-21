e434ca6dbb5fd8f11e31beb4cf96339ea6603e0f
wdower <will@dower.dev>
2023-06-21 15:36:21 -0400

    adding nginx ingress templates -- assuming ingress-nginx was installed via helm separately
    Signed-off-by: wdower <will@dower.dev>

--- start-heimdall2.sh
+++ start-heimdall2.sh
@@ -10,6 +10,26 @@
 
 source .env
 
+if [ -f ./nginx/certs/ssl_certificate.crt ]; then
+	echo "SSL Certificate already exists. if you would like to regenerate your certificates, please delete the files in ./nginx/certs/ and re-run this script."
+else
+	echo "SSL Certificate does not exist, creating self-signed certificate..."
+	echo "Do not use a self-signed certificate in a production environment."
+	echo
+	echo "Generating certificate (Expires in 7 days)..."
+	openssl req -newkey rsa:4096 \
+							-x509 \
+							-sha256 \
+							-days 7 \
+							-nodes \
+							-out nginx/certs/ssl_certificate.crt \
+							-keyout nginx/certs/ssl_certificate_key.key \
+							-subj "/C=US/ST=SelfSigned/L=SelfSigned/O=SelfSigned/OU=SelfSigned"
+fi
+
+echo "Done generating keys"
+
+
 # secrets
 DATABASE_USERNAME="postgres"
 DATABASE_PASSWORD="$(openssl rand -hex 33)"
@@ -21,7 +41,7 @@ NAMESPACE="heimdall"
 # Install heimdall
 helm install heimdall heimdall2 \
     -n $NAMESPACE --create-namespace \
-    -f values.yaml \
+    -f heimdall2/local-values.yaml \
     --set   databaseUsername=$DATABASE_USERNAME \
     --set   databasePassword=$DATABASE_PASSWORD \
     --set   jwtSecret=$JWT_SECRET \
@@ -35,4 +55,7 @@ helm install heimdall heimdall2 \
     --set   oidcUserInfoUrl=$OIDC_USER_INFO_URL \
     --set   oidcClientId=$OIDC_CLIENTID \
     --set   oidcClientSecret=$OIDC_CLIENT_SECRET \
-    --set   externalUrl=$EXTERNAL_URL
+    --set   externalUrl=$EXTERNAL_URL \
+    --set   nginx-ingress.controller.defaultTLS.cert=$NGINX_CRT \
+    --set   nginx-ingress.controller.defaultTLS.key=$NGINX_KEY
+
